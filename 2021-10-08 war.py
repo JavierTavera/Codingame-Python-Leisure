d8276f33fd30f15473e40b88c3e415207aa02ed7
JavierTavera <javieriti@gmail.com>
2021-10-08 00:42:30 -0300

    Update 2021-10-08 war.py

--- 2021-10-08 war.py
+++ 2021-10-08 war.py
@@ -9,6 +9,8 @@ cardp_2 = []
 orderOfWonCards = []
 rounds = 0
 cardsForTheWinner = []
+cardsForTheWinner1 = []
+cardsForTheWinner2 = []
 cardOrder = {"2":1, "3":2, "4":3, "5":4, "6":5, "7":6, "8":7, "9":8, "10":9, "J":11, "Q":12, "K":13, "A":14}
 pat = 0
 
@@ -25,40 +27,46 @@ def card_value(card):
     else:
         return cardOrder[card[:2]]
 
-def wars():
+###
+### Cambiar: li.insert(0, a) is around 5x faster than li = [a] + li
+###
+
+def wars(recur):
     global cardp_1, cardp_2, pat
-    if len(cardp_1) <= 5 or len(cardp_2) <= 5:
+    if len(cardp_1) < 4 or len(cardp_2) < 4:
         print("PAT")
         pat = 1
     else:
-        card1 = cardp_1[len(cardp_1)-5]
-        card2 = cardp_2[len(cardp_2)-5]
-        for i in range(5):
-            cardsForTheWinner.append(cardp_1.pop(len(cardp_1)-1-i))
-        for i in range(5):
-            cardsForTheWinner.append(cardp_2.pop(len(cardp_2)-1-i))
-        if card1 == card2:
-            wars()
-        elif card1 > card2:
-            cardp_1 = cardsForTheWinner + cardp_1
+        card1 = cardp_1[recur]
+        card2 = cardp_2[recur]
+        for i in range(recur + 1):
+            cardsForTheWinner1.append(cardp_1.pop(0))
+        for i in range(recur + 1):
+            cardsForTheWinner2.append(cardp_2.pop(0))
+        if card_value(card1) == card_value(card2):
+            wars(3)
+        elif card_value(card1) > card_value(card2):
+            cardsForTheWinner = cardsForTheWinner1 + cardsForTheWinner2
+            cardp_1 = cardp_1 + cardsForTheWinner
             print(cardp_1, file=sys.stderr, flush=True)
         else:
-            cardp_2 = cardsForTheWinner + cardp_2
+            cardsForTheWinner = cardsForTheWinner1 + cardsForTheWinner2
+            cardp_2 = cardp_2 + cardsForTheWinner
             print(cardp_2, file=sys.stderr, flush=True)
 
 while(len(cardp_1) > 0 and len(cardp_2) > 0 and pat != 1):
     print(rounds + 1, file=sys.stderr, flush=True)
     print(cardp_1, file=sys.stderr, flush=True)
     print(cardp_2, file=sys.stderr, flush=True)
-    if card_value(cardp_1[len(cardp_1)-1]) == card_value(cardp_2[len(cardp_2)-1]):
-        wars()
+    if card_value(cardp_1[0]) == card_value(cardp_2[0]):
+        wars(4)
         cardsForTheWinner.clear()
-    elif card_value(cardp_1[len(cardp_1)-1]) > card_value(cardp_2[len(cardp_2)-1]):
-        cardp_1 = [cardp_1.pop(len(cardp_1)-1)] + cardp_1
-        cardp_1 = [cardp_2.pop(len(cardp_2)-1)] + cardp_1
+    elif card_value(cardp_1[0]) > card_value(cardp_2[0]):
+        cardp_1 = cardp_1 + [cardp_1.pop(0)]
+        cardp_1 = cardp_1 + [cardp_2.pop(0)]
     else:
-        cardp_2 = [cardp_2.pop(len(cardp_2)-1)] + cardp_2
-        cardp_2 = [cardp_1.pop(len(cardp_1)-1)] + cardp_2
+        cardp_2 = cardp_2 + [cardp_1.pop(0)]
+        cardp_2 = cardp_2 + [cardp_2.pop(0)]
     
     rounds += 1
 
