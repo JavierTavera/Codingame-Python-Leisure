9985599efa8815c4a3c6102988ba41ac02f61949
JavierTavera <javieriti@gmail.com>
2021-10-14 00:39:48 -0300

    Update 2021-10-14 Sum of Spiral's Diagonals (as a challenge).py

--- 2021-10-14 Sum of Spiral's Diagonals (as a challenge).py
+++ 2021-10-14 Sum of Spiral's Diagonals (as a challenge).py
@@ -1,15 +1,10 @@
-n = int(input())
-n2 = n
+n2 = n = int(input())
 sum = 0
-nro_actual = 1
-
+actual_number = 1
 while(n2 > 1):
     for i in range(4):
-        sum += nro_actual # Adding the corners
-        nro_actual += n2 - 1 # Calculating the corners
+        sum += actual_number # Adding the corners
+        actual_number += n2 - 1 # Calculating the corners
     n2 -= 2
-
-if n2 == 1:
-    sum += pow(n,2)
-
+if n2 == 1: sum += pow(n,2)
 print(sum)
