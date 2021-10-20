9a9aea0fa860c76e5a025bf46710a9ece4c095d2
JavierTavera <javieriti@gmail.com>
2021-10-20 03:25:34 -0300

    Update 2021-10-20 Death First Search - Episode 2.py

--- 2021-10-20 Death First Search - Episode 2.py
+++ 2021-10-20 Death First Search - Episode 2.py
@@ -18,9 +18,39 @@ for i in range(l):
     link[n1] = link[n1] + [n2]
     link[n2] = link[n2] + [n1]
 
-ei = []
-for i in range(e):
-    ei.append(int(input()))  # the index of a gateway node
+ei = [int(input()) for i in range(e)]
+steps_to_gateway = {ei[i]:-1 for i in range(e)}
+doNotVisitAgain = []
+
+def f_steps_to_gateway(a_node):
+    nextNode = [a_node]
+    tempNodes = []
+    steps = 0
+    while True:
+        print(f"{nextNode = }", file=sys.stderr, flush=True)
+        tempNodes.clear()
+        for nod in nextNode:
+            doNotVisitAgain.append(nod)
+            for nod3 in link[nod]:
+                print(f"{nod3 = }", file=sys.stderr, flush=True)
+                if nod in ei and not nod in sever_link[nod3]:
+                    sever_link[nod3] = sever_link[nod3] + [nod]
+                    sever_link[nod] = sever_link[nod] + [nod3]
+                    print(nod, nod3, file=sys.stderr, flush=True)
+                    return str(nod) + " " + str(nod3)
+        for nod in nextNode:
+            for nod2 in link[nod]:
+                if not nod2 in doNotVisitAgain and not nod2 in nextNode:
+                    print(f"{nod2 = }", file=sys.stderr, flush=True)
+                    tempNodes.append(nod2)
+            nextNode.clear()
+            nextNode = [-1] + tempNodes
+            nextNode.remove(-1)
+            #nextNode.remove(nod)
+
+#ei = []
+#for i in range(e):
+#    ei.append(int(input()))  # the index of a gateway node
 
 sever_link = {i:[] for i in range(n)}
 # game loop
@@ -36,19 +66,15 @@ while True:
             print(nod, si)
             break
 
+# sever a link to the closest gateway
+# if there are 2 gateways with the same amount of steps, sever the one with more posibilities to get in
+
     link_to_gateway2 = True
     if link_to_gateway == -1:
-        for nod in ei:
-            if link_to_gateway2:
-                for nod2 in link[nod]:
-                    if not nod in sever_link[nod2]:# or not nod2 in sever_link:
-                        link_to_gateway2 = False
-                        sever_link[nod] = sever_link[nod] + [nod2]
-                        sever_link[nod2] = sever_link[si] + [nod]
-                        print(nod, nod2)
-                        break
+        print(f_steps_to_gateway(si))
 
     # Write an action using print
     # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
     #print("3 4")
+
