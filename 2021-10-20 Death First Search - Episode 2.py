ae8dc3bcae47d091b67705d53258fa4fe26c44f2
JavierTavera <javieriti@gmail.com>
2021-10-20 21:19:27 -0300

    Update 2021-10-20 Death First Search - Episode 2.py

--- 2021-10-20 Death First Search - Episode 2.py
+++ 2021-10-20 Death First Search - Episode 2.py
@@ -18,6 +18,9 @@ for i in range(l):
     link[n2] = link[n2] + [n1]
 
 ei = [int(input()) for i in range(e)]
+possible_routes = []
+possible_nodes = []
+first_node = []
 #steps_to_gateway = {ei[i]:-1 for i in range(e)}
 doNotVisitAgain = []
 
@@ -28,6 +31,7 @@ def f_steps_to_gateway(a_node):
     while True:
         print(f"{nextNode = }", file=sys.stderr, flush=True)
         tempNodes.clear()
+        possible_routes.clear()
         for nod in nextNode:
             doNotVisitAgain.append(nod)
             print(f"{doNotVisitAgain = }", file=sys.stderr, flush=True)
@@ -40,16 +44,34 @@ def f_steps_to_gateway(a_node):
                     sever_link[nod] = sever_link[nod] + [nod3]
                     print(nod, nod3, file=sys.stderr, flush=True)
                     doNotVisitAgain.clear()
+                    # in here, nod would be the gateway
+                    #possible_routes.append([nod3, nod])
                     return str(nod) + " " + str(nod3)
-                #elif link[nod] in ei and not nod in sever_link[nod3]:
                 else:
                     for knod in ei:
-                        if nod in link[knod] and not nod in sever_link[knod]:
-                            sever_link[knod] = sever_link[nod3] + [nod]
-                            sever_link[nod] = sever_link[nod] + [knod]
+                        if nod in link[knod] and not nod in sever_link[knod] and knod  == nod3: # in here, knod would be the gateway
                             print(nod, knod, file=sys.stderr, flush=True)
                             doNotVisitAgain.clear()
-                            return str(nod) + " " + str(knod)
+                            possible_routes.append([nod, knod])
+                            print(f"{possible_routes = }", file=sys.stderr, flush=True)
+                            #return str(nod) + " " + str(knod)
+        print(f"{possible_routes = }", file=sys.stderr, flush=True)
+        if len(possible_routes) > 0:
+            first_node.clear()
+            for routes in possible_routes:
+                print(f"{routes = }", file=sys.stderr, flush=True)
+                print(f"{first_node = }", file=sys.stderr, flush=True)
+                if not routes[0] in first_node:
+                    possible_nodes.append(routes)
+                    first_node.append(routes[0])
+                else:
+                    sever_link[routes[0]] = sever_link[routes[0]] + [routes[1]]
+                    sever_link[routes[1]] = sever_link[routes[1]] + [routes[0]]
+                    return str(routes[0]) + " " + str(routes[1])
+            sever_link[possible_routes[0][0]] = sever_link[possible_routes[0][0]] + [possible_routes[0][1]]
+            sever_link[possible_routes[0][1]] = sever_link[possible_routes[0][1]] + [possible_routes[0][0]]
+            return str(possible_routes[0][0]) + " " + str(possible_routes[0][1])
+
         for nod in nextNode:
             for nod2 in link[nod]:
                 if not nod2 in doNotVisitAgain and not nod2 in nextNode:
@@ -58,7 +80,6 @@ def f_steps_to_gateway(a_node):
             nextNode.clear()
             nextNode = [-1] + tempNodes
             nextNode.remove(-1)
-            #nextNode.remove(nod)
 
 #ei = []
 #for i in range(e):
@@ -69,23 +90,8 @@ sever_link = {i:[] for i in range(n)}
 while True:
     si = int(input())  # The index of the node on which the Bobnet agent is positioned this turn
     print(f"{si = }", file=sys.stderr, flush=True)
-    link_to_gateway = -1
-    for nod in link[si]:
-        if nod in ei and not nod in sever_link[si]:# or si in sever_link):
-            link_to_gateway = nod
-            sever_link[nod] = sever_link[nod] + [si]
-            sever_link[si] = sever_link[si] + [nod]
-            print(nod, si)
-            break
 
-# sever a link to the closest gateway
-# if there are 2 gateways with the same amount of steps, sever the one with more posibilities to get in
-
-    link_to_gateway2 = True
-    if link_to_gateway == -1:
-        print(f_steps_to_gateway(si))
+    print(f_steps_to_gateway(si))
 
     # Write an action using print
     # To debug: print("Debug messages...", file=sys.stderr, flush=True)
-
-    #print("3 4")
