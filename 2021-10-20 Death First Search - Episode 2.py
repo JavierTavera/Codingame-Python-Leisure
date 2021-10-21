7b858d9155d04af13a6dd069b045ea9e0d3d005b
JavierTavera <javieriti@gmail.com>
2021-10-21 00:06:58 -0300

    Update 2021-10-20 Death First Search - Episode 2.py

--- 2021-10-20 Death First Search - Episode 2.py
+++ 2021-10-20 Death First Search - Episode 2.py
@@ -1,6 +1,7 @@
 #Not yet finished
 import sys
 import math
+#import itertools
 
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
@@ -18,16 +19,59 @@ for i in range(l):
     link[n2] = link[n2] + [n1]
 
 ei = [int(input()) for i in range(e)]
+notVisit = set()
+out_of_loop = [0]
+def find_next_do_gateway(start, nNode):
+    while True:
+        #notVisit = itertools.chain(start, nNode)
+        #notVisit = start + nNode
+        #notVisit.extend(start)
+        #notVisit.extend(nNode) # podría quitarlo
+        notVisit.update(start)
+        notVisit.update(nNode)
+        for nod in start:
+            if not nod in notVisit:
+                counting = 0
+                for gate in ei:
+                    if nod in link[gate]: counting += 1
+                    if counting == 2:
+                        sever_link[gate] = sever_link[gate] + [nod]
+                        sever_link[nod] = sever_link[nod] + [gate]
+                        notVisit.clear()
+                        print(gate, nod)
+                        return True
+        # Retirarse si se exhaustó la lista
+        count2 = 0
+        out_of_loop[0] += 1
+        if out_of_loop[0] > 50:
+            return False
+        for every_node in link:
+            if not every_node in notVisit and not every_node in ei:
+                print(f"{every_node = }", file=sys.stderr, flush=True)
+                count2 += 1
+        if count2 == 0:
+            return False
+        #seguir buscando
+        print(f"{notVisit = }", file=sys.stderr, flush=True)
+        print(f"{start = }", file=sys.stderr, flush=True)
+        for iNode in start:
+            for iNode2 in link[iNode]:
+                print(f"{iNode2 = }", file=sys.stderr, flush=True)
+                if not iNode2 in notVisit:
+                    return find_next_do_gateway([iNode2], nNode)
+
+            
+                    
+        
+
 possible_routes = []
 possible_nodes = []
 first_node = []
-#steps_to_gateway = {ei[i]:-1 for i in range(e)}
 doNotVisitAgain = []
 
 def f_steps_to_gateway(a_node):
     nextNode = [a_node]
     tempNodes = []
-    #steps = 0
     while True:
         print(f"{nextNode = }", file=sys.stderr, flush=True)
         tempNodes.clear()
@@ -68,18 +112,25 @@ def f_steps_to_gateway(a_node):
                     sever_link[routes[0]] = sever_link[routes[0]] + [routes[1]]
                     sever_link[routes[1]] = sever_link[routes[1]] + [routes[0]]
                     return str(routes[0]) + " " + str(routes[1])
+            out_of_loop[0] = 0
+            if find_next_do_gateway(first_node, nextNode):
+                return
             sever_link[possible_routes[0][0]] = sever_link[possible_routes[0][0]] + [possible_routes[0][1]]
             sever_link[possible_routes[0][1]] = sever_link[possible_routes[0][1]] + [possible_routes[0][0]]
             return str(possible_routes[0][0]) + " " + str(possible_routes[0][1])
-
-        for nod in nextNode:
-            for nod2 in link[nod]:
-                if not nod2 in doNotVisitAgain and not nod2 in nextNode:
-                    print(f"{nod2 = }", file=sys.stderr, flush=True)
-                    tempNodes.append(nod2)
             nextNode.clear()
-            nextNode = [-1] + tempNodes
+            nextNode = [-1] + first_node
             nextNode.remove(-1)
+        
+        else:
+            for nod in nextNode:
+                for nod2 in link[nod]:
+                    if not nod2 in doNotVisitAgain and not nod2 in nextNode:
+                        print(f"{nod2 = }", file=sys.stderr, flush=True)
+                        tempNodes.append(nod2)
+                nextNode.clear()
+                nextNode = [-1] + tempNodes
+                nextNode.remove(-1)
 
 #ei = []
 #for i in range(e):
